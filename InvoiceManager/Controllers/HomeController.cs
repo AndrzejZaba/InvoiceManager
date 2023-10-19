b08dd11b65e0b810a8714574d178a364c595d677
AndrzejZaba <andrzejzaba@onet.pl>
2023-10-19 14:10:58 +0200

    Registration form modified

--- InvoiceManager/Controllers/HomeController.cs
+++ InvoiceManager/Controllers/HomeController.cs
@@ -8,11 +8,13 @@ namespace InvoiceManager.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +22,7 @@ namespace InvoiceManager.Controllers
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
